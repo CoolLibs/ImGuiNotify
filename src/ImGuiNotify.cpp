2aa70a187a88b7cabff4e14eaf1670021c7cd1e1
Jules Fouchy <jules.fouchy@ntymail.com>
2024-11-23 16:23:03 +0100

    🧼 Cleanup

--- src/ImGuiNotify.cpp
+++ src/ImGuiNotify.cpp
@@ -8,37 +8,16 @@
 
 namespace ImGuiNotify {
 
-#define NOTIFY_NULL_OR_EMPTY(str) (!str || !strlen(str))
-
 namespace {
 class NotificationImpl {
 public:
-    auto getDefaultTitle() const -> std::string_view
-    {
-        if (!_toast.title.empty())
-            return _toast.title;
+    explicit NotificationImpl(Notification notification)
+        : _notification{std::move(notification)}
+    {}
 
-        switch (_toast.type)
-        {
-        case Type::Success:
-            return "Success";
-        case Type::Warning:
-            return "Warning";
-        case Type::Error:
-            return "Error";
-        case Type::Info:
-            return "Info";
-        default:
-        {
-            assert(false);
-            return "";
-        }
-        }
-    }
-
-    auto getColor() const -> ImVec4
+    auto color() const -> ImVec4
     {
-        switch (_toast.type)
+        switch (_notification.type)
         {
         case Type::Success:
             return get_style().success;
@@ -54,63 +33,69 @@ public:
         }
     }
 
-    auto getIcon() const -> const char*
+    auto icon() const -> const char*
     {
-        switch (_toast.type)
+        switch (_notification.type)
         {
         case Type::Success:
-            return ICON_FA_CIRCLE_CHECK; // Font Awesome 6
+            return ICON_FA_CIRCLE_CHECK;
         case Type::Warning:
-            return ICON_FA_TRIANGLE_EXCLAMATION; // Font Awesome 6
+            return ICON_FA_TRIANGLE_EXCLAMATION;
         case Type::Error:
-            return ICON_FA_CIRCLE_EXCLAMATION; // Font Awesome 6
+            return ICON_FA_CIRCLE_EXCLAMATION;
         case Type::Info:
-            return ICON_FA_CIRCLE_INFO; // Font Awesome 6
+            return ICON_FA_CIRCLE_INFO;
         default:
             assert(false);
-            return nullptr;
+            return "";
         }
     }
 
-    auto getElapsedTime() const
+    auto has_content() const -> bool
     {
-        return std::chrono::steady_clock::now() - *_creationTime;
+        return !_notification.content.empty() || _notification.custom_imgui_content;
     }
-
-    auto has_been_init() const -> bool
+    auto content() const -> std::string const& { return _notification.content; }
+    auto custom_imgui_content() const -> std::function<void()> const& { return _notification.custom_imgui_content; }
+    auto title() const -> std::string const& { return _notification.title; }
+    auto unique_id() const -> std::string const& { return _unique_id; }
+    auto has_been_init() const -> bool { return _creation_time.has_value(); }
+    auto elapsed_time() const
     {
-        return _creationTime.has_value();
+        assert(has_been_init());
+        return std::chrono::steady_clock::now() - *_creation_time;
     }
     auto has_expired() const -> bool
     {
         return has_been_init()
-               && getElapsedTime() > _toast.duration + get_style().fade_in_duration + get_style().fade_out_duration;
+               && elapsed_time() > _notification.duration + get_style().fade_in_duration + get_style().fade_out_duration;
     }
 
     void init_creation_time_ifn()
     {
-        if (_creationTime.has_value())
+        if (_creation_time.has_value())
             return;
-        _creationTime.emplace(std::chrono::steady_clock::now());
+        _creation_time.emplace(std::chrono::steady_clock::now());
     }
 
     void reset_creation_time()
     {
-        if (getElapsedTime() > get_style().fade_in_duration)
-            _creationTime.emplace(std::chrono::steady_clock::now() - get_style().fade_in_duration);
+        if (elapsed_time() > get_style().fade_in_duration)
+            _creation_time.emplace(std::chrono::steady_clock::now() - get_style().fade_in_duration);
     }
 
     auto is_fading_out() const -> bool
     {
-        return has_been_init() && getElapsedTime() > _toast.duration + get_style().fade_in_duration;
+        return has_been_init()
+               && elapsed_time() > _notification.duration + get_style().fade_in_duration;
     }
 
-    auto getFadePercent() const -> float
+    auto fade_percent() const -> float
     {
-        float const elapsed_ms  = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(getElapsedTime()).count());
+        float const elapsed_ms  = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_time()).count());
         float const fade_in_ms  = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(get_style().fade_in_duration).count());
         float const fade_out_ms = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(get_style().fade_out_duration).count());
-        float const duration_ms = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(_toast.duration).count());
+        float const duration_ms = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(_notification.duration).count());
 
         float opacity = 1.f;
 
@@ -122,12 +107,12 @@ public:
         return std::clamp(opacity, 0.f, 1.f);
     }
 
-public:
-    Notification                                         _toast;
-    std::optional<std::chrono::steady_clock::time_point> _creationTime{};
-    float                                                _window_height{};
-    inline static uint64_t                               next_id{0};
-    std::string                                          _uniqueId{std::to_string(next_id++)};
+private:
+    Notification                                         _notification;
+    std::optional<std::chrono::steady_clock::time_point> _creation_time{};
+
+    inline static uint64_t _next_id{0};
+    std::string            _unique_id{std::to_string(_next_id++)};
 };
 } // namespace
 
@@ -139,7 +124,7 @@ static auto notifications() -> std::vector<NotificationImpl>&
 
 void send(Notification notification)
 {
-    notifications().push_back(NotificationImpl{std::move(notification)});
+    notifications().emplace_back(std::move(notification));
 }
 
 static auto ImU32_from_ImVec4(ImVec4 color) -> ImU32
@@ -179,111 +164,79 @@ void render_windows()
     std::erase_if(notifications(), [](NotificationImpl const& toast) {
         return toast.has_expired();
     });
-    ImVec2 const mainWindowSize = ImGui::GetMainViewport()->Size;
 
     float height = 0.f;
-
     for (size_t i = 0; i < std::min(notifications().size(), get_style().render_limit); ++i)
     {
-        NotificationImpl& currentToast = notifications()[i];
+        auto& notif = notifications()[i];
 
-        // Init creation time the first time a toast is shown, because NOTIFY_RENDER_LIMIT might prevent it from showing for a while, and we don't want it to disappear immediately after appearing
-        currentToast.init_creation_time_ifn();
+        notif.init_creation_time_ifn(); // Init creation time the first time a notification is shown, because get_style().render_limit might prevent it from showing for a while, and we don't want it to disappear immediately after appearing
 
-        // Get icon, title and other data
-        const char* icon         = currentToast.getIcon();
-        const char* title        = currentToast._toast.title.c_str();
-        const char* content      = currentToast._toast.content.c_str();
-        const char* defaultTitle = currentToast.getDefaultTitle().data();
-        const float opacity      = currentToast.getFadePercent();
-
-        // Generate new unique name for this toast
-        auto windowName = "##TOAST" + currentToast._uniqueId;
+        float const fade_percent = notif.fade_percent();
 
         // Set notification window position to bottom right corner of the main window, considering the main window size and location in relation to the display
-        ImVec2 mainWindowPos = ImGui::GetMainViewport()->Pos;
-        ImGui::SetNextWindowPos(ImVec2(mainWindowPos.x + mainWindowSize.x - get_style().padding_x, mainWindowPos.y + mainWindowSize.y - get_style().padding_y - height), ImGuiCond_Always, ImVec2(1.0f, 1.0f));
-        ImGui::SetNextWindowSizeConstraints(
-            {get_style().min_width, 0.f}, {FLT_MAX, FLT_MAX}, [](ImGuiSizeCallbackData* data) {
-                data->DesiredSize = {data->DesiredSize.x, data->DesiredSize.y * (*(float*)data->UserData)};
+        ImVec2 const main_window_pos  = ImGui::GetMainViewport()->Pos;
+        ImVec2 const main_window_size = ImGui::GetMainViewport()->Size;
+        ImGui::SetNextWindowPos(
+            ImVec2{
+                main_window_pos.x + main_window_size.x - get_style().padding_x,
+                main_window_pos.y + main_window_size.y - get_style().padding_y - height
             },
-            (void*)&opacity
+            ImGuiCond_Always, ImVec2{1.f, 1.f}
         );
-        // Set notification window flags
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, get_style().border_width);
-        ImVec4 textColor = currentToast.getColor();
-        ImGui::PushStyleColor(ImGuiCol_Border, textColor); // Red color
-                                                           // textColor.w = opacity;
+        ImGui::SetNextWindowSizeConstraints(
+            ImVec2{get_style().min_width, 0.f}, ImVec2{FLT_MAX, FLT_MAX}, [](ImGuiSizeCallbackData* data) {
+                float const fade_percent = *reinterpret_cast<float const*>(data->UserData); // NOLINT(*reinterpret-cast)
+                data->DesiredSize        = ImVec2{data->DesiredSize.x, data->DesiredSize.y * fade_percent};
+            },
+            (void*)&fade_percent // NOLINT(*casting)
+        );
 
-        ImGui::Begin(windowName.c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoFocusOnAppearing);
+        ImGui::PushStyleColor(ImGuiCol_Border, notif.color());
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, get_style().border_width);
+        ImGui::Begin(("##TOAST" + notif.unique_id()).c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoFocusOnAppearing);
 
         // Render over all other windows
         ImGui::BringWindowToDisplayFront(ImGui::GetCurrentWindow());
 
+        // Keep alive if hovered
         if (ImGui::IsWindowHovered())
-            currentToast.reset_creation_time();
+            notif.reset_creation_time();
 
-        // Here we render the toast content
+        // Here we render the content
         {
-            ImGui::PushTextWrapPos(mainWindowSize.x / 3.f); // We want to support multi-line text, this will wrap the text after 1/3 of the screen width
+            ImGui::PushTextWrapPos(main_window_size.x / 3.f); // We want to support multi-line text, this will wrap the text after 1/3 of the screen width
 
-            bool wasTitleRendered = false;
-
+            // Title
             background(get_style().title_background, [&]() {
-                // If an icon is set
-                if (!NOTIFY_NULL_OR_EMPTY(icon))
-                {
-                    // Text(icon); // Render icon text
-                    ImGui::TextColored(textColor, "%s", icon);
-                    wasTitleRendered = true;
-                }
-
-                // If a title is set
-                if (!NOTIFY_NULL_OR_EMPTY(title))
-                {
-                    // If a title and an icon is set, we want to render on same line
-                    if (!NOTIFY_NULL_OR_EMPTY(icon))
-                        ImGui::SameLine();
-
-                    ImGui::Text("%s", title); // Render title text
-                    wasTitleRendered = true;
-                }
-                else if (!NOTIFY_NULL_OR_EMPTY(defaultTitle))
-                {
-                    if (!NOTIFY_NULL_OR_EMPTY(icon))
-                        ImGui::SameLine();
-
-                    ImGui::Text("%s", defaultTitle);
-                    wasTitleRendered = true;
-                }
+                ImGui::TextColored(notif.color(), "%s", notif.icon());
+                ImGui::SameLine();
+                ImGui::TextUnformatted(notif.title().c_str());
             });
 
-            // In case ANYTHING was rendered in the top, we want to add a small padding so the text (or icon) looks centered vertically
-            if (wasTitleRendered && (!NOTIFY_NULL_OR_EMPTY(content) || currentToast._toast.custom_imgui_content))
+            // Content
+            if (notif.has_content())
             {
-                ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 5.f); // Must be a better way to do this!!!!
-            }
+                // Add a small padding after the title
+                ImGui::Dummy({0.f, 5.f});
 
-            // If a content is set
-            if (!NOTIFY_NULL_OR_EMPTY(content))
-            {
-                ImGui::Text("%s", content); // Render content text
-            }
+                if (!notif.content().empty())
+                    ImGui::TextUnformatted(notif.content().c_str());
 
-            if (currentToast._toast.custom_imgui_content)
-                currentToast._toast.custom_imgui_content();
+                if (notif.custom_imgui_content())
+                    notif.custom_imgui_content()();
+            }
 
             ImGui::PopTextWrapPos();
         }
 
-        // Save height for next toasts
-        currentToast._window_height = ImGui::GetWindowHeight();
-        height += currentToast._window_height + get_style().padding_between_notifications_y * opacity;
+        // Update height for next notification
+        height += ImGui::GetWindowHeight() + get_style().padding_between_notifications_y * fade_percent;
 
         // End
         ImGui::End();
-        ImGui::PopStyleColor();
         ImGui::PopStyleVar();
+        ImGui::PopStyleColor();
     }
 }
 
