b49ff7ae5a18b73fa286c1055b84583cb0ac11de
Jules Fouchy <jules.fouchy@ntymail.com>
2024-11-23 17:29:49 +0100

    ✅ Add tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,11 +1,31 @@
-#include <ImGuiNotify/ImGuiNotify.hpp>
-#include <quick_imgui/quick_imgui.hpp>
+#include "ImGuiNotify/ImGuiNotify.hpp"
+#include "quick_imgui/quick_imgui.hpp"
 
 auto main(int argc, char* argv[]) -> int
 {
-    quick_imgui::loop("ImGuiNotify tests", [&]() { // Open a window and run all the ImGui-related code
-        ImGui::Begin("ImGuiNotify tests");
-        ImGui::End();
-        ImGui::ShowDemoWindow();
-    });
+    bool const should_run_imgui_tests = argc < 2 || strcmp(argv[1], "-nogpu") != 0; // NOLINT(*pointer-arithmetic)
+    if (!should_run_imgui_tests)
+        return 0;
+
+    quick_imgui::loop(
+        "ImGuiNotify tests",
+        []() { // Init
+            ImGui::GetIO().Fonts->AddFontDefault();
+            ImGuiNotify::add_icons_to_current_font();
+        },
+        [&]() { // Loop
+            ImGui::Begin("ImGuiNotify tests");
+            if (ImGui::Button("Success"))
+                ImGuiNotify::send({.type = ImGuiNotify::Type::Success, .content = "Hello"});
+            if (ImGui::Button("Warning"))
+                ImGuiNotify::send({.type = ImGuiNotify::Type::Warning, .title = "Warning", .content = "Hello"});
+            if (ImGui::Button("Error"))
+                ImGuiNotify::send({.type = ImGuiNotify::Type::Error, .content = "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"});
+            if (ImGui::Button("Info"))
+                ImGuiNotify::send({.type = ImGuiNotify::Type::Info, .content = "Hello"});
+            ImGui::End();
+
+            ImGuiNotify::render_windows();
+        }
+    );
 }
